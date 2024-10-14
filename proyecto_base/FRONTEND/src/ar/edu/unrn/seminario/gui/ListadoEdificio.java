3695392f851cd5ce179f29c84295ad79ba478784
Paula <agostinellipaula7@gmail.com>
2024-10-14 18:15:25 -0300

    Update ListadoEdificio.java

--- proyecto_base/FRONTEND/src/ar/edu/unrn/seminario/gui/ListadoEdificio.java
+++ proyecto_base/FRONTEND/src/ar/edu/unrn/seminario/gui/ListadoEdificio.java
@@ -5,13 +5,13 @@ import java.awt.EventQueue;
 import ar.edu.unrn.seminario.api.IApi;
 import javax.swing.JFrame;
 
-public class ListadoEdifico {
+public class ListadoEdificio {
 
 	private JFrame frame;
 
 // constructor
 	
-	public ListadoEdifico(IApi api) {
+	public ListadoEdificio(IApi api) {
 		frame = new JFrame("Listado de edificios");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
