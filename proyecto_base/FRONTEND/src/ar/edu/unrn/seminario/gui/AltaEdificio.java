ada80db272decb1f279579c7dfb6519ab4658ded
Paula <agostinellipaula7@gmail.com>
2024-10-14 20:43:38 -0300

    ermine alta edificio y faltan cosas de listado edificio

--- proyecto_base/FRONTEND/src/ar/edu/unrn/seminario/gui/AltaEdificio.java
+++ proyecto_base/FRONTEND/src/ar/edu/unrn/seminario/gui/AltaEdificio.java
@@ -1,13 +1,18 @@
 package ar.edu.unrn.seminario.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.AbstractButton;
+
 
 import ar.edu.unrn.seminario.api.IApi;
 import ar.edu.unrn.seminario.dto.EdificioDTO;
@@ -16,13 +21,10 @@ import ar.edu.unrn.seminario.dto.RecursoDTO;
 public class AltaEdificio {
 	private JFrame frame;	// Ventana principal
 	private JTextField textFieldNombreEdificio;// aca quizas conviene usar un jcomboBox
-	private JTextField textFieldNumeroDeAula;
-	//private JTextField textFieldCpacidadDeAula;
-	private JButton Aceptar; 
-	private JButton Cancelar; 
-	private JButton Modificar;
-	
-	private List<Aula> recursos = new ArrayList<>(); //Lista de Recursos
+	private JTextField textFieldDireccionEdificio;
+	private JButton btnAceptar; 
+	private JButton btnCancelar; 
+	 private JComboBox<String> edificioComboBox;
 	
 	
 	/**
@@ -30,34 +32,71 @@ public class AltaEdificio {
 	 */
 	public AltaEdificio(IApi api) {
 		frame = new JFrame("Alta de edificio");
-		
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
         
         JLabel nombreEdificio = new JLabel("Nombre del edificio");
         nombreEdificio.setBounds(50, 30, 130, 20);
         frame.getContentPane().add(nombreEdificio);
         
         
-        TexField nombreEdificio = new JTextField ();
-        nombreEdificio.setBounds (200,30,180,20);
-        frame.getContentPane().add(nombreEdificio);
+        textFieldNombreEdificio = new JTextField();
+        textFieldNombreEdificio.setBounds(200, 30, 180, 20);
+        frame.getContentPane().add(textFieldNombreEdificio);
+        
+     // Etiqueta para la dirección del edificio
+        JLabel direccionEdificioLabel = new JLabel("Dirección del edificio");
+        direccionEdificioLabel.setBounds(50, 70, 130, 20);
+        frame.getContentPane().add(direccionEdificioLabel);
+
+        // Campo de texto para ingresar la dirección del edificio
+        textFieldDireccionEdificio = new JTextField();
+        textFieldDireccionEdificio.setBounds(200, 70, 180, 20);
+        frame.getContentPane().add(textFieldDireccionEdificio);
         
         JLabel seleccionarEdificio = new JLabel("Seleccione el edificio");
         nombreEdificio.setBounds(50, 30, 130, 20);
         frame.getContentPane().add(nombreEdificio);
         
-        seleccionarComboBox = new JComboBox();
-        seleccionarComboBox.setBound (200,30,180,20);
-        frame.getContentPane().add(seleccionarComboBox);
+     
+ 	 // Combo Box para edificio
+		edificioComboBox = new JComboBox<String>(); // aca no se si hace falta el string 
+		edificioComboBox.setBounds(204, 10, 134, 21); 
+		frame.getContentPane().add(edificioComboBox);
+		
         
-        
-        
-        
-        
-        
-        
-        
-}
+		
+		btnAceptar = new JButton("Aceptar");
+        btnAceptar.setBounds(111, 160, 85, 30);
+        frame.getContentPane().add(btnAceptar);
+	      
+	        btnAceptar.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                String nombre = textFieldNombreEdificio.getText();
+	                String direccion = textFieldDireccionEdificio.getText();
+	                
+	                api.cargarEdificio(nombre , direccion);
+	                
+					frame.setVisible(false);
+					frame.dispose();
+	            }
+	        });
 	
-}
+	                
+	        // Botón para cancelar
+	      btnCancelar = new JButton("Cancelar");
+	      btnCancelar.setBounds(290, 160, 100, 30);
+	      frame.getContentPane().add(btnCancelar);
+	                
+	                // Listener para cerrar la ventana
+	      	btnCancelar.addActionListener(new ActionListener() {
+	      		public void actionPerformed(ActionEvent e) {
+	      				frame.dispose(); // Cierra la ventana
+	      		}
+	          });
+	                
+	             frame.setVisible(true);         
+	          }
+	  
+	}
--- proyecto_base/FRONTEND/src/ar/edu/unrn/seminario/gui/ListadoEdificio.java
+++ proyecto_base/FRONTEND/src/ar/edu/unrn/seminario/gui/ListadoEdificio.java
@@ -1,22 +1,147 @@
 package ar.edu.unrn.seminario.gui;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
 
 import ar.edu.unrn.seminario.api.IApi;
+import ar.edu.unrn.seminario.dto.EdificioDTO;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class ListadoEdificio {
 
 	private JFrame frame;
-
-// constructor
 	
+	private JTextField textFieldNombreDelEdificio;
+	private JTextField textFieldDireccionDelEdificio;
+	private JButton btnModificar; 
+	private JButton btnEliminar; 
+	private JButton btnSalir; 
+	private JTable table;
+	private JPanel contentPane;
+	DefaultTableModel modelo;
+	//private JComboBox<String> edificioComboBox;
+
+
 	public ListadoEdificio(IApi api) {
 		frame = new JFrame("Listado de edificios");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		frame.setVisible (true);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		frame.setContentPane (contentPane);
+
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+
+		table = new JTable();
+		String[] titulos = { "NOMBRE", "DIRECCION"};
+
+		table.addMouseListener(new MouseAdapter() { // esto seria que cuando hago click en un campo activo los botones
+			
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// Habilitar botones
+				habilitarBotones(true);
+
+			}
+		});
+		modelo = new DefaultTableModel(new Object[][] {}, titulos);
+		
+		// Obtiene la lista de usuarios a mostrar
+		List<EdificioDTO> edificios = api.obtenerEdificios();//esta en la api??
+		// Agrega los edificios en el model
+		for (EdificioDTO e : edificios) {
+				modelo.addRow(new Object[] { e.getNombre(), e.getDireccion() });
+			}
+
+		table.setModel(modelo);
+
+		scrollPane.setViewportView(table);
+
+		btnModificar = new JButton("Modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					int opcionSeleccionada = JOptionPane.showConfirmDialog(null,
+							"Estas seguro que desea modificar el edificio?", JOptionPane.YES_NO_OPTION);
+						if (opcionSeleccionada == JOptionPane.YES_OPTION) {
+							String nombreDelEdificio = (String) table.getModel().getValueAt(table.getSelectedRow(), 0);
+
+							api.actualizarEdificio(nombreDelEdificio);//esto tiene que estar en la API
+							actualizarTabla();
+
+						}
+
+					}
+
+				});	
+		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					
+			}
+				});	
+		
+		btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				  frame.dispose(); // Cierra la ventana
+			}	
+
+		});	
+		
+		
+		JPanel pnlBotonesOperaciones = new JPanel();
+		pnlBotonesOperaciones.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		contentPane.add(pnlBotonesOperaciones, BorderLayout.SOUTH);
+		pnlBotonesOperaciones.add(btnModificar);
+		pnlBotonesOperaciones.add(btnEliminar);
+		pnlBotonesOperaciones.add(btnSalir);
+
+		// Deshabilitar botones que requieren tener una fila seleccionada
+		habilitarBotones(false);
+	
+
+	private void habilitarBotones(boolean b) {
+		btnModificar.setEnabled(b);
+		btnEliminar.setEnabled(b);
+
+	}
+
+	private void actualizarTabla() {
+		// Obtiene el model del table
+		DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+		// Obtiene la lista de usuarios a mostrar
+		List<EdificioDTO> edificios = api.obtenerEdificios();
+		// Resetea el model
+		modelo.setRowCount(0);
+
+		// Agrega los usuarios en el model
+		for (EdificioDTO e : edificios) {
+			modelo.addRow(new Object[] { e.getNombre(), e.getDireccion() });
+		}
+
 	}
 
 }
+
+		
