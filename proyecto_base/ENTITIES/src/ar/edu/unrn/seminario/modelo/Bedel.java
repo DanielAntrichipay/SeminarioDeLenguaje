495c87fd2a3ab60415393f53b87394ed821ed92b
Daniel <danielantrichipay20@gmail.com>
2024-10-24 21:24:02 -0300

    Entidad Bedel
    Se creo la entidad Bedel.

---
+++ proyecto_base/ENTITIES/src/ar/edu/unrn/seminario/modelo/Bedel.java
@@ -0,0 +1,13 @@
+package ar.edu.unrn.seminario.modelo;
+
+public class Bedel extends Persona{
+	/*------------------------------- ATRIBUTOS -------------------------------*/
+	
+	/*-------------------------------- MÉTODOS --------------------------------*/
+	
+	/*----------------------------- CONSTRUCTORES -----------------------------*/
+	
+	public Bedel (String unNombre, String unApellido, String unLegajo, Usuario unUsuario) {
+		super( unNombre, unApellido, unLegajo, unUsuario);
+	}
+}
